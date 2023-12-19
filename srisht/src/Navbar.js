8f22603f14018af1157e0fac19ad0e19d0a8c150
Srishti Sharma <147276642+srishtiisharmaa@users.noreply.github.com>
2023-12-19 14:48:12 -0500

    Delete srisht directory

--- srisht/src/Navbar.js
+++
@@ -1,7 +0,0 @@
-export default function Navbar() {
-  return (
-    <header id="header-container">
-      <h3 className="title shimmer">Wordle-anche!</h3>
-    </header>
-  );
-}
--- srisht/src/helpers.js
+++
@@ -1,7 +0,0 @@
-function is5Letters(word) {
-  return word.length === 5
-}
-// is5Letters(word);
-function isAnswer(word) {
-  return word.includes(answer);
-}
